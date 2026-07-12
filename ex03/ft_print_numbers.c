3cca498c1433baf0d92ab56a03761d7d3d574015
Htoowaithaw <htoowaithaw2028@gmail.com>
2026-07-12 17:37:41 +0800

    Finish ex03 practice

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
-void	ft_print_numbers(void)
+void ft_print_numbers(void)
 {
-	char	c;
-
-	c = '0';
-	while (c <= '9')
-	{
-		write(1, &c, 1);
-		c++;
-	}
+    char c; // Declare c variable as in char
+    c = '1'; // Use the character '1' (ASCII 49)
+    while ( c <= '9' )  // 9 is also the literal character printing to terminal
+    {
+        write( 1, &c, 1);
+        c++;
+    }
+    return ;
 }
